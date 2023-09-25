07983b43f9980947212ebacc658f45fa26a7056f
arya-sa <rizkyaryas11@gmail.com>
2023-09-25 10:19:19 +0700

    add image-item component

---
+++ components/image-item.tsx
@@ -0,0 +1,24 @@
+import { cn } from '@/lib/utils'
+import Image, { ImageProps } from 'next/image'
+import * as React from 'react'
+
+interface ImageItemProps extends ImageProps {
+    src: string
+    alt: string
+    className?: string
+}
+
+const ImageItem: React.FC<ImageItemProps> = ({ alt, className, src }) => {
+    return (
+        <Image
+            src={src}
+            alt={alt}
+            width={0}
+            height={0}
+            sizes='100vw'
+            className={cn('w-full h-auto', className)}
+        />
+    )
+}
+
+export default ImageItem
