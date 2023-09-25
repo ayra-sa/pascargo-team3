595364364dbe6ffdf5d6f064b746c2f5308a4406
arya-sa <rizkyaryas11@gmail.com>
2023-09-25 10:20:39 +0700

    add mission component

---
+++ components/mission.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+type Props = {}
+
+const Mission = (props: Props) => {
+  return (
+    <section>
+        <div className="container">
+            mission
+        </div>
+    </section>
+  )
+}
+
+export default Mission
